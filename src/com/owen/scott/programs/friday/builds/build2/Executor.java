4932ef28a9238516a99399459e90bef3af0bb0e9
Owen Scott <owens3364@gmail.com>
2020-02-07 10:37:04 -0600

    Made Executor

---
+++ src/com/owen/scott/programs/friday/builds/build2/Executor.java
@@ -0,0 +1,24 @@
+package com.owen.scott.programs.friday.builds.build2;
+
+import com.owen.scott.annotations.InProgress;
+
+import java.util.List;
+
+@InProgress
+public class Executor implements Runnable {
+    @Override
+    public void run() {
+        NumberSeries fibonacciSeries = new FibonacciNumberSeries();
+        NumberSeries pyramidalSeries = new PyramidalNumberSeries();
+        NumberSeries triangularSeries = new TriangularNumberSeries();
+        System.out.println(formatIntList(fibonacciSeries.getSeries(15)));
+        System.out.println(formatIntList(pyramidalSeries.getSeries(15)));
+        System.out.println(formatIntList(triangularSeries.getSeries(15)));
+    }
+
+    private String formatIntList(List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        list.forEach(sb::append);
+        return sb.toString();
+    }
+}
