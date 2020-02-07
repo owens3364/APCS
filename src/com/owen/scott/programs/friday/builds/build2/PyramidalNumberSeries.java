1657a4d5ebf9faf576ccefcab68ac29177900906
Aliki Kassioti <alikikassioti@Alikis-MacBook-Pro.local>
2020-02-07 11:08:49 -0600

    added pyrimidal class

--- src/com/owen/scott/programs/friday/builds/build2/PyramidalNumberSeries.java
+++ src/com/owen/scott/programs/friday/builds/build2/PyramidalNumberSeries.java
@@ -7,4 +7,17 @@ public class PyramidalNumberSeries implements NumberSeries {
     public List<Integer> getSeries(int length) {
         return null;
     }
+   int pentagon_pyrimidal (int n)
+   {
+       int sum = 0;
+       //run loop from 1 to n
+       for (int i = 1; i <= n; i++)
+       {
+           //get nth pentagonal number
+           int p = (3 * i * i - i) /2;
+           //add to sum
+           sum = sum +p;
+       }
+       return sum;
+   }
 }
